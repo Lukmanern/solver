dbbd66a5b7dd3279517f81915718788234ea93a5
LukmanE22 <unsurlukman@gmail.com>
2023-01-06 15:43:57 +0700

    add more comments and readme in StringParser problems

---
+++ StringParser/README.md
@@ -0,0 +1,3 @@
+### About
+
+parseString takes a string in the format "key1: value1, key2: value2, ..." and returns a map[string]string where each key is mapped to its corresponding value.
--- StringParser/main.go
+++ StringParser/main.go
@@ -15,16 +15,30 @@ func main() {
 }
 
 func parseString(str string) map[string]string {
+	// Initialize an empty map to store the key-value pairs.
 	result := make(map[string]string)
 
+	// Split the input string on the comma character to 
+	// get a slice of key-value pairs.
 	pairs := strings.Split(str, ",")
+	
+	// Iterate over the slice of pairs.
 	for _, pair := range pairs {
+		// Split the pair on the colon character 
+		// to get the key and value.
 		keyValue := strings.Split(pair, ":")
+		// Check if the split resulted 
+		// in a key and value.
 		if len(keyValue) == 2 {
-			// trims space " "
-			result[strings.TrimSpace(keyValue[0])] = strings.TrimSpace(keyValue[1])
+			// Trim leading and trailing white space 
+			// from the key and value.
+			key := strings.TrimSpace(keyValue[0])
+			value := strings.TrimSpace(keyValue[1])
+			// Add the key-value pair to the map.
+			result[key] = value
 		}
 	}
 
 	return result
 }
+
