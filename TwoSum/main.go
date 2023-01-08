754c283704aa8490c25a7c2a23034521355d0a7e
LukmanE22 <unsurlukman@gmail.com>
2023-01-08 22:28:46 +0700

    make some change, make it more efective for TwoSum Problem

--- TwoSum/main.go
+++ TwoSum/main.go
@@ -1,30 +1,53 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	nums := []int{
+		-1,0,1,4,6,7,8,
+	}
+	target := 9
+	fmt.Println(twoSum(nums, target)) // (2,6) => 1 + 8 = 9
 }
 
 func twoSum(nums []int, target int) []int {
-	var i, j, v1, v2 int
+	// Initialize variables to store 
+	// the indices of the elements
+	var i, j int
+	// Initialize a flag to indicate 
+	// when to break out of the loop
 	var isBreak bool = false
+	// Get the length of the array
+	var ln int = len(nums)
 
-	if len(nums) == 2 {
+	// Check if the length of the array is 2
+	if ln == 2 {
+		// Return the indices if 
+		// the length of the array is 2
 		return []int{0, 1}
 	}
-	for i, v1 = range nums {
-		for j, v2 = range nums {
-			if j <= i {
-				continue
-			}
-			if v1+v2 == target {
+
+	// Loop over the array elements
+	for i = 0; i < ln; i++ {
+		// Loop over the remaining elements
+		for j = i + 1; j < ln; j++ {
+			// Check if the current element and 
+			// the next element add up to the target
+			if nums[i]+nums[j] == target {
+				// Set the flag to true if 
+				// the target has been found
 				isBreak = true
 				break
 			}
 		}
+		// Break out of the outer loop 
+		// if the target has been found
 		if isBreak {
 			break
 		}
 	}
 
+	// Return the indices of the 
+	// elements that add up to the target
 	return []int{i, j}
 }
