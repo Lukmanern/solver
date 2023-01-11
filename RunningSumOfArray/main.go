c24cdd1eb88c9e4ca31a9e2a50ba515a921acbc9
LukmanE22 <unsurlukman@gmail.com>
2023-01-11 23:22:52 +0700

    add runningSumOfArray problem

---
+++ RunningSumOfArray/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4} // [1, 3, 6, 10]
+	result := runningSum(nums)
+	fmt.Println(result)
+}
+
+// runningSum is a function that takes in a slice of integers
+// and returns a new slice with the running sum of the original slice.
+func runningSum(nums []int) []int {
+	// Iterate through the 
+	// input slice starting at index 1
+	for i := 1; i < len(nums); i++ {
+		// Add the current value to the previous 
+		// value and update the current index
+		nums[i] += nums[i-1]
+	}
+	return nums
+}
