d01cc945c52ad93a8dad13725b74d872e36771da
LukmanE22 <unsurlukman@gmail.com>
2023-01-12 22:51:30 +0700

    add comments in 3Sum problem

--- 3Sum/main.go
+++ 3Sum/main.go
@@ -6,18 +6,33 @@ import (
 )
 
 func threeSum(nums []int) [][]int {
+	// Initialize an empty slice 
+	// to store the result &
+	// sort the input slice
 	var res [][]int
 	sort.Ints(nums)
+
 	for i := 0; i < len(nums)-2; i++ {
+		// Skip the current element 
+		// if it's a duplicate
 		if i > 0 && nums[i] == nums[i-1] {
 			continue
 		}
+		// Init left and right pointers
 		l, r := i+1, len(nums)-1
+		// Iterate through the remaining elements 
+		// to the right of the current element
 		for l < r {
+			// Calculate the sum of 
+			// the current triplet
 			s := nums[i] + nums[l] + nums[r]
+			// If the sum is less than 0, 
+			// increment the left pointer
 			if s < 0 {
 				l++
 			} else if s > 0 {
+				// If the sum is greater than 0, 
+				// decrement the right pointer
 				r--
 			} else {
 				res = append(res, []int{nums[i], nums[l], nums[r]})
