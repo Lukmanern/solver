d6f820ea31f64f108b4c1b06e577cacbeec917a6
LukmanE22 <unsurlukman@gmail.com>
2023-01-12 22:43:22 +0700

    add 3Sum problem

---
+++ 3Sum/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func threeSum(nums []int) [][]int {
+	var res [][]int
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		l, r := i+1, len(nums)-1
+		for l < r {
+			s := nums[i] + nums[l] + nums[r]
+			if s < 0 {
+				l++
+			} else if s > 0 {
+				r--
+			} else {
+				res = append(res, []int{nums[i], nums[l], nums[r]})
+				for l < r && nums[l] == nums[l+1] {
+					l++
+				}
+				for l < r && nums[r] == nums[r-1] {
+					r--
+				}
+				l++
+				r--
+			}
+		}
+	}
+	return res
+}
+
+func main() {
+	nums := []int{-1, 0, 1, 2, -1, -4, 5}
+	fmt.Println(threeSum(nums))
+}
