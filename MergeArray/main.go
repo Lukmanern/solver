a59d01b6396f8a895de601f3dd4a47581baeee84
LukmanE22 <unsurlukman@gmail.com>
2023-01-17 20:33:07 +0700

    fix MergeArray problem

--- MergeArray/main.go
+++ MergeArray/main.go
@@ -1,7 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	m := 3
+	nums2 := []int{2, 5, 6}
+	n := 3
+
+	merge(nums1, m, nums2, n)
+	fmt.Println(nums1) // result : 1 2 2 3 5 6
 }
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
