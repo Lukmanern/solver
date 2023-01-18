50891daf7fbee03d48e8628ae13f6e11e2767763
LukmanE22 <unsurlukman@gmail.com>
2023-01-19 00:30:03 +0700

    add some comments to sortColor problem

--- SortColors/main.go
+++ SortColors/main.go
@@ -12,17 +12,37 @@ func main() {
 }
 
 func sortColors(nums []int) {
+    	// Initialize pointers for left 
+	// and right sides of the array
 	left, right := 0, len(nums)-1
+	
+    	// Initialize pointer 
+	// for current element
 	idx := 0
+	
+    	// Loop through the array until 
+	// current pointer reaches the end
 	for idx <= right {
 		if nums[idx] == 0 {
+			// Swap current element with element 
+			// at left pointer if it's 0
 			nums[idx], nums[left] = nums[left], nums[idx]
+			
+            	// Move left pointer to the right
 			left++
+			
+            	// Move current pointer to the right
 			idx++
 		} else if nums[idx] == 2 {
+            	// Swap current element with element 
+			// at right pointer if it's 2
 			nums[idx], nums[right] = nums[right], nums[idx]
+			
+            	// Move right pointer to the left
 			right--
 		} else {
+            	// Move current pointer to the 
+			// right if element is 1
 			idx++
 		}
 	}
