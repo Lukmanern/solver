dc512dfda879b9251edb833cadb951bc3e841486
LukmanE22 <unsurlukman@gmail.com>
2023-01-22 19:14:41 +0700

    add dataset to run CoinChange

--- CoinChange/main.go
+++ CoinChange/main.go
@@ -1,6 +1,37 @@
 package main
 
+import "fmt"
+
 func main() {
+	// Test case 1
+	coins := []int{1, 2, 5}
+	amount := 11
+	result := coinChange(coins, amount)
+	fmt.Println(result) // Expects 3
+
+	// Test case 2
+	coins  = []int{2}
+	amount = 3
+	result = coinChange(coins, amount)
+	fmt.Println(result) // Expects -1
+
+	// Test case 3
+	coins  = []int{1}
+	amount = 0
+	result = coinChange(coins, amount)
+	fmt.Println(result) // Expects 0
+
+	// Test case 4
+	coins  = []int{186, 419, 83, 408}
+	amount = 6249
+	result = coinChange(coins, amount)
+	fmt.Println(result) // Expects 20
+
+	// Test case 5
+	coins  = []int{1, 3, 5}
+	amount = 8
+	result = coinChange(coins, amount)
+	fmt.Println(result) // Expects 2
 
 }
 
