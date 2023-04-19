62a2309941f26c1c5bcfc77f1cbea57381c9ebb2
LukmanE22 <unsurlukman@gmail.com>
2023-04-20 03:16:44 +0700

    autocommit

--- DivideTwoIntegers/main.go
+++ DivideTwoIntegers/main.go
@@ -1,8 +1,53 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
-func divide(dividend int, divisor int) int {
+func main() {
+	var res_1, res_2 int
+	dataset := [][2]int{
+		{10, 3},
+		{11, 3},
+		{-1, 3},
+		{-10, 3},
+		{0, 3},
+	}
+
+	for _, data := range dataset {
+		res_1 = divide_1(data[0], data[1])
+		res_2 = divide_2(data[0], data[1])
+		fmt.Println("divide_1 :", res_1)
+		fmt.Println("divide_2 :", res_2)
+		fmt.Println() // enter
+
+		if res_1 != res_2 {
+			panic("error : result of divide_1 != divide_2")
+		}
+	}
+}
+
+func divide_1(dividend int, divisor int) int {
+	if dividend == 0 {
+		return 0
+	}
+
+	var result int = dividend / divisor
+
+	max32 := math.MaxInt32
+	min32 := math.MinInt32
+
+	if result >= max32 {
+		return max32
+	} else if result <= min32 {
+		return min32
+	}
+
+	return result
+}
+
+func divide_2(dividend int, divisor int) int {
 	// Handle division by zero
 	if divisor == 0 {
 		return math.MaxInt32
